ee4a76c5fe5c05f1862674185401850358564651
wevsty <ty@wevs.org>
2018-02-28 14:14:57 +0800

    first commit

---
+++ std_rand.h
@@ -0,0 +1,21 @@
+﻿#ifndef __STD_RAND_CLASS_H__
+#define __STD_RAND_CLASS_H__
+
+#include <QObject>
+//#include <stdlib.h>
+//#include <stdio.h>
+//#include <time.h>
+#include <string>
+#include <random>
+
+class std_rand
+{
+public:
+    unsigned int n_rand_seed;
+    std_rand();
+    int rand_int();
+    std::string rand_std_string(int maxlen = 8);
+
+};
+
+#endif // __STD_RAND_CLASS_H__
